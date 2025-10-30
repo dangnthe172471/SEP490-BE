8221902871a54e80412659401b86eb7f31bccd59
nvchien190 <nvchien1904@gmail.com>
2025-10-30 16:29:08 +0700

    doctor schedule

---
+++ SEP490_BE/SEP490_BE.API/Controllers/DoctorControllers/DoctorScheduleController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SEP490_BE.BLL.IServices;
+using SEP490_BE.BLL.IServices.IDoctorServices;
+
+namespace SEP490_BE.API.Controllers.DoctorControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorScheduleController : ControllerBase
+    {
+        private readonly IDoctorScheduleService _service;
+
+        public DoctorScheduleController(IDoctorScheduleService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("doctor-active-schedule-range/{doctorId}")]
+        public async Task<IActionResult> GetDoctorActiveScheduleInRange(
+               int doctorId,
+               [FromQuery] DateOnly? startDate,
+               [FromQuery] DateOnly? endDate)
+        {
+            try
+            {
+                var data = await _service.GetDoctorActiveScheduleInRangeAsync(
+                    doctorId,
+                    startDate ?? default,
+                    endDate ?? default
+                );
+
+                if (data == null || data.Count == 0)
+                {
+                    return Ok(new
+                    {
+                        message = "Không có lịch làm việc cho bác sĩ trong khoảng thời gian này."
+                    });
+                }
+
+                return Ok(data);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Lỗi hệ thống: " + ex.Message });
+            }
+        }
+    }
+}
---
+++ SEP490_BE/SEP490_BE.BLL/Services/DoctorServices/DoctorScheduleService.cs
@@ -0,0 +1,38 @@
+﻿using SEP490_BE.BLL.IServices.IDoctorServices;
+using SEP490_BE.DAL.DTOs;
+using SEP490_BE.DAL.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEP490_BE.BLL.Services.DoctorServices
+{
+    public class DoctorScheduleService : IDoctorScheduleService
+    {
+        private readonly IDoctorRepository _repo;
+
+        public DoctorScheduleService(IDoctorRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<List<DoctorActiveScheduleRangeDto>> GetDoctorActiveScheduleInRangeAsync(int doctorId, DateOnly startDate, DateOnly endDate)
+        {
+            if (doctorId <= 0)
+                throw new ArgumentException("DoctorId không hợp lệ.");
+
+            
+            if (startDate == default || endDate == default)
+            {
+                var today = DateOnly.FromDateTime(DateTime.Now);
+                startDate = new DateOnly(today.Year, today.Month, 1);
+                endDate = new DateOnly(today.Year, today.Month, DateTime.DaysInMonth(today.Year, today.Month));
+            }
+
+            var schedules = await _repo.GetDoctorActiveScheduleInRangeAsync(doctorId, startDate, endDate);
+
+            return schedules ?? new List<DoctorActiveScheduleRangeDto>();
+        }
+    }
+}
